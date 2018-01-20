47b0fce609fadee67c664b809fc2dd585cfb4224
Aswin Bhim Nath <aswismail@gmail.com>
2018-01-20 12:49:23 +0530

    first commit

---
+++ app/src/main/java/com/bhim/www/chatcat/MainActivity.java
@@ -0,0 +1,39 @@
+package com.bhim.www.chatcat;
+
+import android.content.Intent;
+import android.icu.text.DateFormat;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseListAdapter<ChatMessage> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    private void displayChatMessages() {
+
+    }
+    @Override
+
+}
